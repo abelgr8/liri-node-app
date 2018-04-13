3042eb85d0a7cc2338785ab8223d5797831c3ccf
Abel Georges <abelgr8@gmail.comgit config --global user.name Abel>
2018-04-12 23:44:04 -0400

    omdb addition

--- keys.js
+++ keys.js
@@ -12,3 +12,5 @@ exports.spotify = {
     secret: process.env.SPOTIFY_SECRET
 };
 
+exports.omdb = process.env.OMDB_API_KEY;
+
--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var keys = require("./keys.js");
 
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require('request');
 
 
 //Twitter==============================================================//
@@ -21,6 +22,7 @@ var recentTweets = function () {
             // console.log(tweets);
             for (var i = 0; i < tweets.length; i++) {
                 console.log("tweeted by: @" + tweets[i].user.screen_name + "\n" + tweets[i].text + "\n" + tweets[i].created_at);
+                log("-------------------------------------", "blue");
             }
         }
     });
@@ -49,21 +51,39 @@ var getSpotify = function (songName) {
         var songs = data.tracks.items;
         for (var i = 0; i < songs.length; i++) {
             console.log(i);
-            console.log("Artist(s): " + songs[i].artists.map(artistName) + "\nSong name: " + songs[i].name + "\nAlbum: " + songs[i].album.name);
+            console.log("Artist(s): " + songs[i].artists.map(artistName) + "\nSong name: " + songs[i].name + "\nAlbum: " + songs[i].album.name + "\nPreview song: " + songs[i].preview_url);
             console.log("================================================")
         }
     });
 }
 
+// Request============================================================//
+
+var movReq = function (movieName) {
+
+    request("http://www.omdbapi.com/?t=" + movieName + "&apikey=" + keys.omdb, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            
+            var movieData = JSON.parse(body);
+            
+            console.log('Title:'+ movieData.Title + "\nYear: " + movieData.Year + "\nRated: " + movieData.Rated + "\nIMDB Rating: " + movieData.imdbRating + "\nRotten Tomatoes Rating: " + movieData.Ratings[1].Value + "\nCountry: " + movieData.Country + "\nLanguage: " + movieData.Language + "\nPlot: " + movieData.Plot + "\nActors: " + movieData.Actors + "\nWebsite: " + movieData.Website);
+        }
+    })
+}
+
+
 ///searches
 
 var pick = function (caseData, functionData) {
     switch (caseData) {
-        case "tweets":
+        case "my-tweets":
             recentTweets();
             break;
-        case "spotify":
+        case "spotify-this-song":
             getSpotify(functionData);
+            break;
+        case "movie-this":
+            movReq(functionData);
             break;
         default:
             log('⚆ _ ⚆ LIRI does not compute', 'red');
